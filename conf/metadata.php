2e6ddfb672d86009cd393b85c9f86d91507de976
Michael Große <grosse@cosmocode.de>
2017-09-13 12:06:27 +0200

    feat: add pluginwizard skeleton

---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the dropfiles plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+
+//$meta['fixme'] = array('string');
+
---
+++ lang/en/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * english language file for dropfiles plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+// keys need to match the config setting name
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
