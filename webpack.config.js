9dfc020b79c61fad7502a95e3ccc99d5cba636d2
Michael Große <grosse@cosmocode.de>
2017-10-10 18:09:39 +0200

    chore: add linting with stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 /* global process */
+var StyleLintPlugin = require('stylelint-webpack-plugin');
 
 // fix for https://github.com/webpack/webpack/issues/2537
 if (process.argv.indexOf('-p') !== -1) {
@@ -22,5 +23,13 @@ module.exports = {
         ],
     },
     plugins: [
+        new StyleLintPlugin({
+            configFile: '.stylelintrc',
+            context: '.',
+            files: '*.less',
+            failOnError: false,
+            quiet: false,
+            syntax: 'less',
+        }),
     ],
 };
